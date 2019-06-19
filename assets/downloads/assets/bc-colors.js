d228fbddbdf54fafcc4cf32fde3709fc87234abb
rebeccaroddenberry <rebecca.roddenberry@bcbsnc.com>
2019-06-19 10:59:54 -0400

    Overwrite all files

---
+++ assets/downloads/assets/bc-colors.js
@@ -0,0 +1,117 @@
+'use strict';
+
+const red = Object.freeze({
+	base: '#bf2e1b',
+	$50: '#f7e6e4',
+	$100: '#ecc0bb',
+	$200: '#df978d',
+	$300: '#d26d5f',
+	$400: '#c94d3d',
+	$500: '#bf2e1b',
+	$600: '#b92918',
+	$700: '#b12314',
+	$800: '#a91d10',
+	$900: '#9b1208'
+});
+
+const orange = Object.freeze({
+	base: '#cb632b',
+	$50: '#f9ece6',
+	$100: '#efd0bf',
+	$200: '#e5b195',
+	$300: '#db926b',
+	$400: '#d37a4b',
+	$500: '#cb632b',
+	$600: '#c65b26',
+	$700: '#be5120',
+	$800: '#b8471a',
+	$900: '#ac3510'
+});
+
+const yellow = Object.freeze({
+	base: '#e8b417',
+	$50: '#fcf6e3',
+	$100: '#f8e9b9',
+	$200: '#f4da8b',
+	$300: '#efcb5d',
+	$400: '#ebbf3a',
+	$500: '#e8b417',
+	$600: '#e5ad14',
+	$700: '#e2a411',
+	$800: '#de9c0d',
+	$900: '#d88c07'
+});
+
+const green = Object.freeze({
+	base: '#79ad65',
+	$50: '#eff5ed',
+	$100: '#d7e6d1',
+	$200: '#bcd6b2',
+	$300: '#a1c683',
+	$400: '#8db97c',
+	$500: '#79ad65',
+	$600: '#71a65d',
+	$700: '#669c53',
+	$800: '#5c9349',
+	$900: '#498337'
+});
+
+const blue = Object.freeze({
+	base: '#0e92c7',
+	$50: '#d8f2fc',
+	$100: '#b3e5fa',
+	$200: '#8ed9f7',
+	$300: '#57c7f3',
+	$400: '#20b4ef',
+	$500: '#0e92c7',
+	$600: '#0d85b5',
+	$700: '#0a6a90',
+	$800: '#084f6b',
+	$900: '#053447'
+});
+
+const brown = Object.freeze({
+	base: '#5d4e3f',
+	$50: '#eceae8',
+	$100: '#cecac5',
+	$200: '#aea79f',
+	$300: '#8e8379',
+	$400: '#75695c',
+	$500: '#5d4e3f',
+	$600: '#554739',
+	$700: '#4b3d31',
+	$800: '#413529',
+	$900: '#30251b'
+});
+
+const black = Object.freeze({
+	base: '#3b3b3f',
+	$50: '#e7e7e8',
+	$100: '#c4c4c5',
+	$200: '#9d9d9f',
+	$300: '#767679',
+	$400: '#58585c',
+	$500: '#3b3b3f',
+	$600: '#353539',
+	$700: '#2d2d31',
+	$800: '#262629',
+	$900: '#19191b'
+});
+
+const misc = Object.freeze({
+	navLogo: '#232f36',
+	navSecondary: '#28353d',
+	nav: '#2f3e47',
+	navExpandHover: '#394750'
+});
+
+module.exports = {
+	red: red,
+	orange: orange,
+	yellow: yellow,
+	green: green,
+	blue: blue,
+	brown: brown,
+	black: black,
+	misc: misc
+};
